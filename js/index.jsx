9755b7bb68333cc429bd5b63980f386ba03c989d
Steven Baum <stevenbaum@berkeley.edu>
2016-10-13 15:30:42 -0700

    Add files via upload

--- js/index.jsx
+++ js/index.jsx
@@ -1,1 +1,107 @@
+// Multi-Touch Attribution Display for Hanna Andersson
 
+// Set Credentialss
+MP.api.setCredentials('9b4f82b0d853dc32417d17f4f6847464');
+
+import React from 'react';
+import {render} from 'react-dom';
+import MultiTouchAttribution from './multiTouchAttribution.jsx';
+
+
+class App extends React.Component {
+
+	// Initialize App with one state property, an array of top attribution flows
+	constructor(props) {
+		super(props);
+		this.state = {
+			topAttributions: [],
+		}
+	}
+
+	// Run queries to pull data on multi-touch attribution,
+	// stored as 'UTM_Sources' list property of the "Complete Purchase" event,
+	// which also has a 'Total Charge' property for the purchase amount
+	// Param: journeyCount - Type: Number - Max amount of journeys to return
+	queryMTA(journeyCount) {
+		var today = new Date();
+		var d = today.getDay().toString();
+		var m = (today.getMonth() + 1).toString();
+		var y = today.getFullYear().toString();
+		var todayString = y + '-' + m + '-' + d;
+
+		var params = {
+			from_date: '2016-07-01',
+			to_date: todayString
+		}
+
+		var app = this;
+
+		// Run JQL query
+		MP.api.jql(function main() {
+			return Events({
+				from_date: params.from_date,
+				to_date: params.to_date,
+				event_selectors: [{event: 'Complete Purchase'}]
+			})
+
+			.groupBy(['properties.UTM_Sources'], function(accumulators, items) {
+				var values = { count: 0, revenue: 0 };
+
+				for (var i = 0; i < accumulators.length; i++) {
+					values.count += accumulators[i].count;
+					values.revenue += accumulators[i].revenue;
+				}
+
+				values.count += items.length;
+				for (var i = 0; i< items.length; i++) {
+					values.revenue += items[i].properties['Total Charge'];
+				}
+				return values;
+			})
+			}, params)
+
+			.done(function(results) {
+				// Get top [journeyCount] customer journeys by total revenue,
+				// with the stipulation that the journey is only 2 or 3 steps
+				var topAttributions = [];
+
+				// Sort in descending order
+				results.sort(function(a, b) {
+					return b.value.revenue - a.value.revenue;
+				});
+
+				// Insert top journeys into array, set array to as part of app state
+				topAttributions = [];
+				for (var i = 0; i < results.length; i++) {
+					var customerFlow = results[i].key;
+					if (customerFlow.length >= 2 && customerFlow.length <= 3) {
+						topAttributions.push(results[i]);
+					}
+					if (topAttributions.length >= journeyCount) {
+						break;
+					}
+				}
+
+				app.setState({ topAttributions: topAttributions });
+			});
+	}
+
+	// When component loads, query for specified number of top attributions
+	componentDidMount() {
+		this.queryMTA(10);
+	}
+
+	render() {
+		var bodyStyle = {
+			fontFamily: 'Source Sans Pro, Helvetica'
+		}
+
+		return(
+			<div style={bodyStyle}>
+				<MultiTouchAttribution attributionList={this.state.topAttributions} />
+			</div>
+		);
+	}
+}
+
+render(<App />, document.getElementById('root'));
---
+++ js/multiTouchAttribution.jsx
@@ -0,0 +1,170 @@
+// Import React & React's render() method
+import React from 'react';
+import {render} from 'react-dom';
+
+/*
+	-- MultiTouchAttribution --
+
+	A three-column table showing customer journeys across UTM channels,
+	grouping channel sequences together and showing resulting conversion amounts & Value
+
+	|______COLUMN 1______|______COLUMN 2______|______COLUMN 3______|
+	|   (Channel Path)   | (# of Conversions) | ($ from Conversion)|
+*/
+
+/* PROPS:
+	'attributionList':
+		Type: Array
+		Array of attribution events, each are objects of the form
+			{
+				key:[
+						"Some Attribution Source A",
+						"Some Attribution Source B",
+						...
+						...
+					],
+				value: {
+						count: [Number],
+						revenue: [Number]
+				} 
+			}
+*/
+
+class MultiTouchAttribution extends React.Component {
+
+	render() {
+		/////    CSS    /////
+		var trStyle = {
+			borderBottom: '#e0e0e0 2px solid'
+		}
+
+		var tdStyle = {
+			padding: '18px'
+		}
+
+		var thStyle = {
+			padding: '8px',
+			minWidth: '200px'
+		}
+
+		var tdNumberStyle = {
+			padding: '18px',
+			textAlign: 'center'
+		}
+
+
+		// Iterate over attribution flows list, create table row elements
+		var attributionList = this.props.attributionList;
+		var tableRows = [];
+		for(var i = 0; i < attributionList.length; i++) {
+			
+			var sequence = attributionList[i].key;
+			var count = attributionList[i].value.count.toLocaleString();
+			var revenue = attributionList[i].value.revenue.toLocaleString();
+
+			tableRows.push(
+				<tr style={trStyle} className="table-row">
+					<td style={tdStyle}><AttributionSequence sequence={sequence} /></td>
+					<td style={tdNumberStyle}>{count}</td>
+					<td style={tdNumberStyle}>{revenue}</td>
+				</tr>
+			);
+		}
+
+		return(
+			<div>
+				<table>
+						<colgroup span="3"></colgroup>
+						<tr style={trStyle}>
+							<th style={thStyle}>Attribution Channel Path</th>
+							<th style={thStyle}>Number of Conversions</th>
+							<th style={thStyle}>Total Revenue</th>
+						</tr>
+						{tableRows}
+				</table>
+			</div>
+		);
+	}
+}
+
+export default MultiTouchAttribution;
+
+
+/*
+	-- AttributionSequence --
+
+	Color-coded Attribution sequences
+*/
+/* PROPS:
+	'sequence':
+		Type: Array of Strings representing a customer journey
+		across multiple attribution sources
+*/
+
+class AttributionSequence extends React.Component {
+
+	render() {
+		/////     CSS     /////
+		var styleMapping = {
+			"Facebook": {
+				backgroundColor: '#495d88',
+			},
+			"Twitter": {
+				backgroundColor: '#55baec',
+			},
+			"Email": {
+				backgroundColor: '#82bf91'
+			},
+			"Google AdWords": {
+				backgroundColor: '#c94547',
+			},
+			"Organic Search": {
+				backgroundColor: '#d6c25c'
+			},
+			"Referral": {
+				backgroundColor: '#b78cd8'
+			}
+		}
+
+		// Style applicable to ALL attribution elements
+		var generalStyle = {
+			padding: '4px 7px 4px 7px',
+			borderRadius: '3px',
+			color: 'white'
+		}
+
+		// Style '>' arrows that direct customer Journey
+		var arrowStyle = {
+			padding: '0px 6px 0px 6px',
+			color: '#bdbdbd',
+			fontWeight: 'bold'
+		}
+
+
+		var sequenceChain = [];
+		for (var i = 0; i < this.props.sequence.length; i++) {
+			var attributionName = this.props.sequence[i];
+			var attributionStyle = styleMapping[attributionName];
+
+			// Merge attribution-specific style with general attribution style
+			var mergedStyle = Object.assign({}, generalStyle, attributionStyle);
+
+			sequenceChain.push(
+				<span style={mergedStyle}>
+					{attributionName}
+				</span>
+			);
+
+			// Append '>' to link items together
+			if (i < this.props.sequence.length - 1) {
+				sequenceChain.push(<span style={arrowStyle}>></span>);
+			}
+		}
+
+		return(
+			<div>
+				{sequenceChain}
+			</div>
+		);
+	}
+}
